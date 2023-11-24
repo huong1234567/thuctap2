87aedbdc6f81bd9ea861fdcb83d3756213ab74e1
huong1234567 <huongdtpd06182@fpt.edu.vn>
2023-11-24 16:37:28 +0700

    add source

---
+++ src/main/java/poly/edu/AsgApplication.java
@@ -0,0 +1,20 @@
+package poly.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.*;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
+public class AsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsgApplication.class, args);
+	}
+
+}
